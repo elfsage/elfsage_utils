0de8ecf5017b917613058e923905831f0785c381
Кац Андрей Валерьевич <akats@alfabank.ru>
2023-07-24 10:57:13 +0400

    Added methods to load/save json

---
+++ elfsage/io/json.py
@@ -0,0 +1,13 @@
+import json
+
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    return data
+
+
+def save_json(data, file_path):
+    with open(file_path, 'r', encoding='urf-8') as f:
+        json.dump(data, f, indent=2)
