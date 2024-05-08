095ca6256363e5ba516648d67585c5060a6dc55e
Кац Андрей Валерьевич <akats@alfabank.ru>
2024-05-08 15:26:44 +0400

    Added mask_to_poly, transform_perspective CV methods

--- elfsage/CV/utils.py
+++ elfsage/CV/utils.py
@@ -1,10 +1,11 @@
-from elfsage.images import to_grey
 import cv2
+import numpy as np
+from elfsage.images import to_grey
 
 
 def mask_to_rect(mask, eps=0.05):
     mask = to_grey(mask)
-    thresh = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)[1]
+    _, thresh = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contour = max(contours, key=cv2.contourArea)
     epsilon = eps*cv2.arcLength(contour, True)
@@ -14,7 +15,49 @@ def mask_to_rect(mask, eps=0.05):
     return rect
 
 
+def mask_to_poly(mask, eps=0.05):
+    mask = to_grey(mask)
+    _, thresh = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contour = max(contours, key=cv2.contourArea)
+    epsilon = eps*cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+
+    return np.squeeze(approx)
+
+
 def crop_by_rect(image, rect):
     x, y, w, h = rect
 
     return image[y:y+h, x:x+w]
+
+
+def transform_perspective(image, pts):
+    def order_points(pts):
+        rect = np.zeros((4, 2), dtype=np.float32)
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+
+        return rect
+
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    width_a = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    width_b = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    max_width = max(int(width_a), int(width_b))
+    height_a = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    height_b = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    max_height = max(int(height_a), int(height_b))
+    dst = np.array([
+        [0, 0],
+        [max_width - 1, 0],
+        [max_width - 1, max_height - 1],
+        [0, max_height - 1]], dtype=np.float32)
+    mat = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, mat, (max_width, max_height))
+
+    return warped
