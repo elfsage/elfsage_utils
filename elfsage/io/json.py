1e6c7485179993957bc7fda69bc198fbd8ceb8d2
Elfsage <elfsage@mail.ru>
2024-05-12 14:16:53 +0400

    Updgraded json functions

--- elfsage/io/json.py
+++ elfsage/io/json.py
@@ -1,18 +1,35 @@
 import json
 from pathlib import Path
+from typing import Union
 
 
-def load_json(file_path, create=False):
-    if not Path(file_path).is_file() and create:
-        with open(file_path, 'w', encoding='utf-8') as f:
-            json.dump({}, f, indent=2)
+def load_json(
+        file_path: Union[str, Path],
+        create: bool = False,
+        default: Union[dict, list] = None
+) -> Union[dict, list]:
+    assert not create or default is not None, 'If create is true, default value must be provided'
 
-    with open(file_path, 'r', encoding='utf-8') as f:
+    if not isinstance(file_path, Path):
+        file_path = Path(file_path)
+
+    if not file_path.is_file():
+        if create:
+            with file_path.open('w', encoding='utf-8') as f:
+                json.dump(default, f, indent=2)
+
+        if default is not None:
+            return default
+
+    with file_path.open('r', encoding='utf-8') as f:
         data = json.load(f)
 
     return data
 
 
-def save_json(data, file_path):
-    with open(file_path, 'w', encoding='utf-8') as f:
+def save_json(data: Union[dict, list], file_path: Union[str, Path]):
+    if not isinstance(file_path, Path):
+        file_path = Path(file_path)
+
+    with file_path.open('w', encoding='utf-8') as f:
         json.dump(data, f, indent=2)
